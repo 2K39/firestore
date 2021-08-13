008a0511181a8757ca5380d52f22100e9d5f0d51
2K39 <ahmed.yousif.k@gmail.com>
2021-08-13 14:43:32 +0300

    test

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Container, Row, Col, Button , Alert} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function about(props) {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                <Alert>
+                    About
+                </Alert>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
